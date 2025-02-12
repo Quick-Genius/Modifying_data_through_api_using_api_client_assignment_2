d17eff05b437aeb5131349aca73bd7af70efd0a0
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2025-02-12 21:33:15 +0530

    Completed the assignment

--- schema.js
+++ schema.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
 const MenuItemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
+    name: { 
+        type: String,
+         required: true 
+        },
+    description: { 
+        type: String 
+    },
+    price: {
+         type: Number, required: true 
+        },
 });
 
 const MenuItem = mongoose.model("MenuItem", MenuItemSchema); // ✅ Correct way
